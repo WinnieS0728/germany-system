e32e2ff671af3a0a3a5bec61de84c686df6d8830
WinnieS0728 <116339731+WinnieS0728@users.noreply.github.com>
2023-07-12 12:03:14 +0800

    Updates

--- assets/coming-795cf96e.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-8c0dfc83.js";const r=()=>o.jsx("h1",{children:"coming soon..."});export{r as default};
---
+++ assets/edit-078cc3f6.js
@@ -0,0 +1,14 @@
+import{n as c,b as l,j as e,N as r,r as n,m as i,o as d,p as o,O as h}from"./index-3afbd829.js";import{H as x}from"./header-e274b21a.js";const m=({className:t})=>{const{t:s}=l(["settingPage"]);return e.jsx(e.Fragment,{children:e.jsx("nav",{className:t,children:e.jsxs("ul",{className:"flex gap-2 p-2",children:[e.jsx(r,{end:!0,to:"tx",children:s("nav.tx")}),e.jsx(r,{end:!0,to:"threshold",children:s("nav.threshold")}),e.jsx(r,{end:!0,to:"store",children:s("nav.store achieve")}),e.jsx(r,{end:!0,to:"osom",children:s("nav.osom achieve")})]})})})},j=c(m)`
+    background-color: ${t=>t.theme.color.navBgc};
+
+    a {
+        border: 1px solid ${t=>t.theme.color.white};
+        color: ${t=>t.theme.color.white};
+        border-radius: .5rem;
+        padding: .2em .5em;
+
+        &.active {
+            background-color: ${t=>t.theme.color.navActive};
+        }
+    }
+`,a=n.lazy(()=>i(()=>import("./coming-18f9910d.js"),["./coming-18f9910d.js","./index-3afbd829.js","./index-1c24e182.css"],import.meta.url)),u=n.lazy(()=>i(()=>import("./threshold-dcc58c17.js"),["./threshold-dcc58c17.js","./index-3afbd829.js","./index-1c24e182.css","./main-39f6d9a0.js"],import.meta.url)),g=()=>{const{t}=l(["settingPage"]);return e.jsxs(e.Fragment,{children:[e.jsx(x,{title:t("title")}),e.jsx(j,{}),e.jsx(n.Suspense,{fallback:e.jsx("h1",{children:"欸你等一下啦"}),children:e.jsxs(d,{children:[e.jsx(o,{path:"tx",element:e.jsx(a,{})}),e.jsx(o,{path:"threshold",element:e.jsx(u,{})}),e.jsx(o,{path:"store",element:e.jsx(a,{})}),e.jsx(o,{path:"osom",element:e.jsx(a,{})})]})}),e.jsx(h,{})]})};export{g as default};
